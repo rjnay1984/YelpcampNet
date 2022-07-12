4a9cc5ed203b390d35badc52b7681e715b1a8e6a
RJ Nay <rjnay1984@gmail.com>
2022-07-12 15:47:05 -0500

    create appuser and identity context

---
+++ src/Infrastructure/Identity/AppIdentityDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace YelpcampNet.Infrastructure;
+
+public class AppIdentityDbContext : IdentityDbContext<ApplicationUser>
+{
+  public AppIdentityDbContext(DbContextOptions options) : base(options)
+  {
+  }
+
+  protected override void OnModelCreating(ModelBuilder builder)
+  {
+    base.OnModelCreating(builder);
+  }
+}
